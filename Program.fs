26dc66fe82b93224c485a92a736e5d9348989658
Edgar Nova <ragnarok540@gmail.com>
2021-07-31 16:01:44 -0500

    first image 2

--- Program.fs
+++ Program.fs
@@ -10,23 +10,18 @@ let generatePixel i j x =
     let r = (float i / 255.0) * x
     let g = (float (255 - j) / 255.0) * x
     let b = 0.25 * x
-    let a =[int r; int g; int b]
+    let a = [int r; int g; int b]
     $"{a.[0]} {a.[1]} {a.[2]}\n"
-
-let createArray length1 length2 =
-    let a2d = Array2D.create length1 length2 255.999
-    a2d
 
 [<EntryPoint>]
 let main argv =
     saveTextFile "out.ppm" "P3\n256 256\n255\n"
-    let arr = createArray 256 256
-    let arr2 =
-        arr
+    let arr =
+        Array2D.create 256 256 255.999
         |> Array2D.mapi generatePixel
 
     for i in 0 .. 255 do
         printfn $"Scanlines remaining {256 - i}"
         for j in 0 .. 255 do
-            appendText "out.ppm" arr2.[j, i]
+            appendText "out.ppm" arr.[j, i]
     0 
