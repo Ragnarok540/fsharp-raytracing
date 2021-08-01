1a71a37e85af0190b46a2d9060aa93783f9f7c22
Edgar Nova <ragnarok540@gmail.com>
2021-07-31 22:19:11 -0500

    first image 4

--- Program.fs
+++ Program.fs
@@ -1,5 +1,15 @@
 open System.IO
 
+type Vec3 (x:float, y:float, z:float) =
+    new () = Vec3(0.0, 0.0, 0.0)
+    member this.X = x
+    member this.Y = y
+    member this.Z = z
+    member this.Sum(other:Vec3) =
+        Vec3(this.X + other.X, this.Y + other.Y, this.Z + other.Z)
+    member this.Product(f:float) =
+        Vec3(this.X * f, this.Y * f, this.Z * f)
+
 let saveTextFile fileName text =
     File.WriteAllText(fileName, text)
 
@@ -13,12 +23,18 @@ let generatePixel i j x =
     let a = [int r; int g; int b]
     $"{a.[0]} {a.[1]} {a.[2]}\n"
 
-[<EntryPoint>]
-let main argv =
+let firstImage =
     saveTextFile "out.ppm" "P3\n256 256\n255\n"
     let arr =
         Array2D.create 256 256 255.999
         |> Array2D.mapi generatePixel
         |> Array2D.iter (fun x -> appendText "out.ppm" x)
+    arr
 
+[<EntryPoint>]
+let main argv =
+    //firstImage
+    let v1 = Vec3(1.0, 2.0, 3.0)
+    let v2 = v1.Sum(Vec3(3.0, 2.0, 1.0))
+    printfn "%f %f %f" v2.X v2.Y v2.Z
     0 
