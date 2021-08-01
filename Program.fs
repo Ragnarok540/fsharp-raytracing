0a514be185c2adc5b8955198e9f0a02cd4b1af84
Edgar Nova <ragnarok540@gmail.com>
2021-08-01 15:18:14 -0500

    gradiente

--- Program.fs
+++ Program.fs
@@ -1,45 +1,5 @@
 open System.IO
-
-type Vec3 (x:float, y:float, z:float) =
-    let lengthSquared = x * x + y * y + z * z
-    new () = Vec3(0.0, 0.0, 0.0)
-    member this.X = x
-    member this.Y = y
-    member this.Z = z
-    member this.Add(other:Vec3) =
-        Vec3(this.X + other.X, this.Y + other.Y, this.Z + other.Z)
-    member this.Sub(other:Vec3) =
-        Vec3(this.X - other.X, this.Y - other.Y, this.Z - other.Z)
-    member this.Mul(f:float) =
-        Vec3(this.X * f, this.Y * f, this.Z * f)
-    member this.Mul2(other:Vec3) =
-        Vec3(this.X * other.X, this.Y * other.Y, this.Z * other.Z)
-    member this.Div(f:float) =
-        Vec3(this.X * (1.0 / f), this.Y * (1.0 / f), this.Z * (1.0 / f))
-    member this.Dot(other:Vec3) =
-        this.X * other.X + this.Y * other.Y + this.Z * other.Z
-    member this.Cross(other:Vec3) =
-        Vec3(this.Y * other.Z - this.Z * other.Y,
-             this.Z * other.X - this.X * other.Z,
-             this.X * other.Y - this.Y * other.X)
-    member this.LengthSquared() =
-        lengthSquared
-    member this.Length() =
-        sqrt lengthSquared
-    member this.UnitVector() =
-        this.Div(this.Length())
-    member this.WriteColor() =
-        $"{int this.X} {int this.Y} {int this.Z}\n"
-
-type Point3 = Vec3
-
-type Color = Vec3
-
-type Ray (origin:Point3, direction:Vec3) =
-    member this.Origin = origin
-    member this.Direction = direction
-    member this.At(t:float) =
-        this.Origin.Add(this.Direction.Mul(t))
+open Types
 
 let saveTextFile fileName text =
     File.WriteAllText(fileName, text)
@@ -47,6 +7,41 @@ let saveTextFile fileName text =
 let appendText fileName text =
     File.AppendAllText(fileName, text)
 
+let rayColor (r:Ray) (x:float) : Color =
+    let unitDirection = r.Direction.UnitVector()
+    let t = 0.5 * unitDirection.Y + 1.0
+    let c1 = Color(1.0, 1.0, 1.0).Mul(1.0 - t)
+    let c2 = Color(0.5, 0.7, 1.0).Mul(t)
+    let c3 = c1.Add(c2)
+    let c3X = c3.Mul(x)
+    c3X
+
+let gradient i j x =
+    // Image
+    let aspectRatio = 16.0 / 9.0
+    let imageWidth = 400
+    let imageHeight = int (float imageWidth / aspectRatio)
+    // Camera
+    let viewportHeight = 2.0
+    let viewportWidth = aspectRatio * viewportHeight
+    let focalLenght = 1.0
+
+    let origin = Point3()
+    let horizontal = Vec3(viewportWidth, 0.0, 0.0)
+    let vertical = Vec3(0.0, viewportHeight, 0.0)
+    let lowerLeftCorner = origin.Sub(horizontal.Div(2.0))
+                                .Sub(vertical.Div(2.0))
+                                .Sub(Vec3(0.0, 0.0, focalLenght))
+    
+    let u = float j / float (imageWidth - 1)
+    let v = float (imageHeight - i) / float (imageHeight - 1)
+    let ray = Ray(origin, lowerLeftCorner.Add(horizontal.Mul(u)
+                                         .Add(vertical.Mul(v)
+                                         .Sub(origin))))
+    let rc = rayColor ray x
+    rc.WriteColor()
+
+(*
 let generatePixel i j x =
     let pixelColor = Color(float j / 255.0, float (255 - i) / 255.0, 0.25)
     let pixelColorX = pixelColor.Mul(x)
@@ -59,12 +54,22 @@ let firstImage =
         |> Array2D.mapi generatePixel
         |> Array2D.iter (fun x -> appendText "out.ppm" x)
     arr
+*)
+
+let secondImage =
+    saveTextFile "out2.ppm" "P3\n400 225\n255\n"
+    let arr =
+        Array2D.create 400 225 255.999
+        |> Array2D.mapi gradient
+        |> Array2D.iter (fun x -> appendText "out2.ppm" x)
+    arr
 
 [<EntryPoint>]
 let main argv =
     //firstImage
-    let v1 = Vec3(1.0, 1.0, 0.0)
-    let v2 = v1.Cross(Vec3(1.0, 0.0, 1.0))
-    let v3 = v2.UnitVector()
-    printfn "%f %f %f" v3.X v3.Y v3.Z
+    secondImage
+    //let v1 = Vec3(1.0, 1.0, 0.0)
+    //let v2 = v1.Cross(Vec3(1.0, 0.0, 1.0))
+    //let v3 = v2.UnitVector()
+    //printfn "%f %f %f" v3.X v3.Y v3.Z
     0 
